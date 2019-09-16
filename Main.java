34c3e1ab30f6b00be0ba3dfc1cdf328478558f49
baooom <54126971+baooom@users.noreply.github.com>
2019-09-16 20:22:32 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,7 +6,10 @@ public class Main {
 
 	public static void main(String args[])
 	{
-		System.out.print("Celsius 35 is Fahrenheit degree ");
-		System.out.println((9/5)*35+32);
+		double radius;
+		double area;
+		radius = 20;
+		area = radius * radius *3.14159;
+		System.out.println("the area for the circle of radius "+radius + " is "+area);
 	}
 }
