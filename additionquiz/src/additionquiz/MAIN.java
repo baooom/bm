cfcc89bffd689c62cd6ad12ce8c1f3a0536be1a2
baooom <54126971+baooom@users.noreply.github.com>
2019-12-29 19:03:26 +0800

    Add files via upload

---
+++ additionquiz/src/additionquiz/MAIN.java
@@ -0,0 +1,17 @@
+package additionquiz;
+
+import java.util.Scanner;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = (int)(System.currentTimeMillis()%10);
+		int number2 = (int)(System.currentTimeMillis()/10%10);
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is "+number1 + " + "+number2+"? ");
+		int ans=input.nextInt();
+		System.out.println(number1+" + "+number2+" = "+ans+" is "+(number1+number2==ans));
+	}
+
+}
